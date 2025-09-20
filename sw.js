4e72c5bf8410ad663ceaf391596e59b427e4ddc0
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-09-20 16:28:49 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,50 @@
+// sw.js
+const CACHE_NAME = 'play-cache-v1';
+const URLS_TO_CACHE = [
+  '/Play.js',
+  '/Play.wasm',
+  '/_headers',
+  '/favicon.ico',
+  '/index.html',
+  '/manifest.json',
+  '/static/js/main.caabe80b.js',
+  '/static/css/main.5a525eca.css'
+];
+
+// Install: pre-cache all core assets
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+});
+
+// Activate: clean up old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+});
+
+// Fetch: serve from cache, fallback to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return (
+        response ||
+        fetch(event.request).then(fetchRes => {
+          // Optionally cache new requests
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchRes.clone());
+            return fetchRes;
+          });
+        })
+      );
+    })
+  );
+});
