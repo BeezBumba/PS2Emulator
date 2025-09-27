20c63d4e54d4fcc226aa5ebb73a2a70bfc0ed147
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-09-27 09:10:47 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // sw.js
-const CACHE_NAME = 'play-cache-v4';
+const CACHE_NAME = 'play-cache-v5';
 const URLS_TO_CACHE = [
   '/Play.js',
   '/Play.wasm',
@@ -8,7 +8,7 @@ const URLS_TO_CACHE = [
   '/play-512.png',
   '/index.html',
   '/manifest.json',
-  '/static/js/main.45ba3ecb.js',
+  '/static/js/main.1deadf99.js',
   '/static/css/main.5a525eca.css'
 ];
 
