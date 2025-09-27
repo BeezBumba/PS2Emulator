82ca7bc53dc4389b3718219564f9d3f36bfba17b
BeezBumba <130922034+BeezBumba@users.noreply.github.com>
2025-09-27 08:44:45 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,7 +8,7 @@ const URLS_TO_CACHE = [
   '/play-512.png',
   '/index.html',
   '/manifest.json',
-  '/static/js/main.1deadf99.js',
+  '/static/js/main.2caea14b.js',
   '/static/css/main.5a525eca.css'
 ];
 
